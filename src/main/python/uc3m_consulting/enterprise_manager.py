7815dca8a34296c542545ef3269d4c9c20d49154
Yehan <yehans5178@gmail.com>
2026-03-24 14:09:17 +0100

    Min code to pass TC1

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,5 +1,5 @@
 """Module """
-
+from .enterprise_project import EnterpriseProject
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
@@ -16,4 +16,7 @@ class EnterpriseManager:
                          project_description: str, department: str,
                          date: str, budget: float):
         """Registers a new project for a company"""
-        pass
+        enterprise_project = EnterpriseProject(
+            company_cif, project_achronym, project_description,
+            department, date, budget)
+        return enterprise_project.project_id
